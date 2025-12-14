1a768c25fc1dde46bea3f4b0e8836bddf93c23ef
Mike Zakany <mzakany@gmail.com>
2025-12-14 16:33:42 -0500

    fix: speed up nav scrolling

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -24,7 +24,8 @@ window.addEventListener('scroll', (ev) => {
 
 // Smooth scroll
 var scroll = new SmoothScroll('#navbar-navlist a', {
-    speed: 300,
+    speed: 100,
+    speedAsDuration: true,
     offset: 60
 });
 
